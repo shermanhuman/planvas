9b3546e483b85d566b673ab1dce227e904ea573b
Sherman Boyd <sherman.boyd@gmail.com>
2016-02-03 00:58:26 -0700

    HTML template coming together, ditched bootstrap, some project cleanup.

--- plan2html.js
+++ plan2html.js
@@ -9,63 +9,66 @@ var fs = require('fs');
 var Handlebars = require('handlebars');
 
 marked.setOptions({
-  gfm: true
+    gfm: true
 });
 
-var leantemplatein = fs.readFileSync('presentation.html', 'utf8');
+var leantemplate = fs.readFileSync('presentation.html', 'utf8');
 
-if (process.argv.length <= 2){
-  console.log(`  No arguments supplied.  Give me some MD files to munch.  
-  Maybe try using the filenames of what you want converted as arguments.  
+if (process.argv.length <= 2) {
+    console.log(`  No arguments supplied.  Give me some MD files to munch.
+  Maybe try using the filenames of what you want converted as arguments.
   As many as you want, seperated by spaces.`)
-}else{
+} else {
 
-for(i=2; i < process.argv.length; i++){
-  val =process.argv[i];
-  console.log('Converting ' + val + ' to HTML.');
-  var htmlfilename = val + '.html';
-  var infile = fs.readFile(val, 'utf8', function(err,data){
-    if(err) throw err;
+    for (i = 2; i < process.argv.length; i++) {
+        val = process.argv[i];
+        console.log('Converting ' + val + ' to HTML.');
+        var htmlfilename = val + '.html';
+        var infile = fs.readFile(val, 'utf8', function (err, data) {
+            if (err) throw err;
 
-    var parsed = parse(data);
-    var planvas = htmlify(parsed);
-    var template = Handlebars.compile(leantemplate);
-    var converted = leantemplate(planvas);
-    fs.writeFile(htmlfilename, converted, function(err){
-      if(err) {
-        throw err;
-      }
-      else{
-        console.log('Saved as ' +  htmlfilename);
-      }
-    })
-  })
-}
+            var parsed = parse(data);
+            var planvas = htmlify(parsed);
+            //console.log('leantemplate = ' + leantemplate);
+            var template = Handlebars.compile(leantemplate);
+            //console.log('planvas = ' + planvas);
+            var converted = template(planvas);
+            console.log('converted = ' + converted);
+            fs.writeFile(htmlfilename, converted, function (err) {
+                if (err) {
+                    throw err;
+                }
+                else {
+                    console.log('Saved as ' + htmlfilename);
+                }
+            })
+        })
+    }
 };
 
-var parse = function(data){
+var parse = function (data) {
     textarray = data.split('##');
- 
+
     var planvas = {
-    problem: '##' + textarray[1],
-    existingalternatives: '##' + textarray[2],
-    solution: '##' + textarray[3],
-    keymetrics: '##' + textarray[4],
-    uniquevalueproposition: '##' + textarray[5],
-    highlevelconcept: '##' + textarray[6],
-    unfairadvantage: '##' + textarray[7],
-    channels: '##' + textarray[8],
-    customersegments: '##' + textarray[9],
-    earlyadopters: '##' + textarray[10],
-    coststructure: '##' + textarray[11],
-    revenuestreams: '##' + textarray[12]
-};
-  //console.log(planvas);
+        problem: '##' + textarray[1],
+        existingalternatives: '##' + textarray[2],
+        solution: '##' + textarray[3],
+        keymetrics: '##' + textarray[4],
+        uniquevalueproposition: '##' + textarray[5],
+        highlevelconcept: '##' + textarray[6],
+        unfairadvantage: '##' + textarray[7],
+        channels: '##' + textarray[8],
+        customersegments: '##' + textarray[9],
+        earlyadopters: '##' + textarray[10],
+        coststructure: '##' + textarray[11],
+        revenuestreams: '##' + textarray[12]
+    };
+    //console.log(planvas);
     return planvas;
 }
 
-var htmlify = function(object){
-    for (var prop in object){
+var htmlify = function (object) {
+    for (var prop in object) {
         object[prop] = marked(object[prop]);
     }
     //console.log(object);
--- test.js
+++
@@ -1,3 +0,0 @@
-var marked = require('marked');
-console.log(marked('I am using __markdown__.'));
-// Outputs: <p>I am using <strong>markdown</strong>.</p>
